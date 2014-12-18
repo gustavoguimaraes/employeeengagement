eba6291d5e6a50b7dcfff7565f7f49d4adc8c4ba
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2014-12-18 23:26:15 +0100

    Add more tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(current_user)
-    unless current_user.company_admin?
+    unless current_user.is_a?(AdminUser)
       dashboard_path
     end
   end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,5 @@
 class DashboardController < ApplicationController
   def index
     @sanity_test = "This is the Dashboard Bitches!"
-    @user_company = current_user.company.name
   end
 end
