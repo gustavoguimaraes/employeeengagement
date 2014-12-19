1ba55c279f11d0ab2f9369ff6da89307664d4d8a
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2014-12-19 15:57:20 +0100

    default admin after log in path to admin dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(current_user)
-    admin_root_path if current_user.is_a?(AdminUser)
+    admin_dashboard_path if current_user.is_a?(AdminUser)
     dashboard_path
   end
 
