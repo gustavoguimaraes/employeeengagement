c285ff5caa6912c2bedefb0b86d3f753bd458c7c
Maximilianoho <maximho20@gmail.com>
2015-02-13 14:57:21 +0100

    fix posts layout and show posts according to the user role

--- app/controllers/users_dashboard_controller.rb
+++ app/controllers/users_dashboard_controller.rb
@@ -5,5 +5,10 @@ class UsersDashboardController < ApplicationController
   def index
     redirect_to admin_dashboard_path if current_admin_user
     @user_posts = Post.where.not(state: Post.states[:rejected]).order(created_at: :desc)
-  end
+  	if current_user.role == "company_admin"
+  		@user_posts = Post.where(state: Post.states[:pending]).order(created_at: :desc)
+  	else
+  		@user_posts = Post.where(state: Post.states[:approved]).order(created_at: :desc)
+ 		end
+	end
 end
