697f24d889a495d426ba4cc00ca951eeb0d34328
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2015-03-30 21:16:56 +0200

    sign up on the landing page if user is not signedin

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
-    redirect_to dashboard_path if user_signed_in?
+    return redirect_to dashboard_path if user_signed_in?
+    redirect_to new_user_registration_path
   end
 end
